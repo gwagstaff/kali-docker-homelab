d86859832e5bc4f80611c5596ee4a156ecf2ad61
braunbearded <braunbearded1@gmail.com>
2022-02-09 23:21:08 +0100

    moved user specific stuff to here

--- default-config/userstartup.sh
+++ default-config/userstartup.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
 
-# Do your setup specific stuff like overriding env variables here
+# Do your user specific stuff like overriding env variables here
 
-# docker specific env
-export TMPDIR=/tmp
-export FAKETIME="+1h"
+# lunarvim
+export PATH=~/.npm-global/bin:$PATH
+[ ! -d "$HOME/.config/lvim" ] && mkdir -p $HOME/.npm-global && npm config set prefix "$HOME/.npm-global" && wget -O /tmp/lunar-install.sh https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh && bash /tmp/lunar-install.sh && rm /tmp/lunar-install.sh && echo "vim.opt.relativenumber = true" >> "$HOME/.config/lvim/config.lua"
+npm list --depth 1 -g neovim > /dev/null || npm install -g neovim
+pip3 show pynvim > /dev/null || sudo python3 -m pip install pynvim
 
-#mkdir -p "/home/kali/.config/vim" "/home/kali/.config/ranger" "/home/kali/.cache/java/.ZAP"
+# install prettier lsd
+if ! command -v lsd > /dev/null; then
+  wget -O /tmp/lsd.deb "$(curl -s https://api.github.com/repos/Peltoche/lsd/releases/latest | jq -r '.assets[].browser_download_url' | grep 'lsd_.*amd64')" 
+  sudo apt install /tmp/lsd.deb 
+  rm /tmp/lsd.deb
+fi
+
+# get ranger conf
+[ ! -f "$HOME/.config/ranger/rc.conf" ] && mkdir -p "$HOME/.config/ranger/" && curl https://gist.githubusercontent.com/braunbearded/de021eda8704ccc98c19e12a15d57ca8/raw/c0b374b0c23582acda72d0a0b4cc5cbfd7a8729e/rc.conf > "$HOME/.config/ranger/rc.conf"
+
+echo "# empty" > "$HOME/.zshrc"
 
 echo ""
 echo "#########################################################################"
