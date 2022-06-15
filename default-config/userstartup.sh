170cb3a5b0eb611f4c552cabcf69805557efd946
braunbearded <braunbearded1@gmail.com>
2022-06-16 01:10:30 +0200

    use nvchad instead of lunarvim

--- default-config/userstartup.sh
+++ default-config/userstartup.sh
@@ -2,79 +2,86 @@
 
 # Do your user specific stuff like overriding env variables here
 
-# lunarvim
-export PATH=~/.npm-global/bin:$PATH
-if [ ! -d "$HOME/.config/lvim" ]; then
-  mkdir -p $HOME/.npm-global && \
-  npm config set prefix "$HOME/.npm-global" && \
-  npm -g install yarn && \
-  wget -O /tmp/lunar-install.sh https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh && \
-  yes "y" | bash /tmp/lunar-install.sh && \
-  rm /tmp/lunar-install.sh && \
-
-  cat >> "$HOME/.config/lvim/config.lua" << EOL
--- ##################################################
--- Run :PackerInstall on first run to install plugins
--- ##################################################
-
-local formatters = require "lvim.lsp.null-ls.formatters"
-formatters.setup {
-  { command = "shfmt", filetypes = { "sh" } },
-}
-
-local linters = require "lvim.lsp.null-ls.linters"
-linters.setup {
-  { command = "shellcheck", filetypes = { "sh" } },
-}
-
-vim.opt.relativenumber = true
-
-lvim.plugins = {
-  {"phaazon/hop.nvim"},
-  {"f-person/git-blame.nvim"},
-  {"windwp/nvim-ts-autotag"},
-  {"folke/trouble.nvim", cmd = "TroubleToggle"},
-  {"folke/todo-comments.nvim"},
-  {"norcalli/nvim-colorizer.lua"}
-}
-
-local _, hop = pcall(require, "hop")
-hop.setup()
-vim.api.nvim_set_keymap("", "s", ":HopChar2<cr>", { silent = true })
-vim.api.nvim_set_keymap("", "S", ":HopWord<cr>", { silent = true })
-
-lvim.builtin.treesitter.autotag = {
-  enable = true,
-  filetypes = {'html', 'xml', 'javascript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'rescript'},
-}
-
--- local _, autotag = pcall(require, "nvim-ts-autotag")
--- autotag.setup()
-
-local _, todo_comments = pcall(require, "todo-comments")
-todo_comments.setup()
-
-local _, colorizer = pcall(require, "colorizer")
-colorizer.setup({ "*" }, {
-    RGB = true, -- #RGB hex codes
-    RRGGBB = true, -- #RRGGBB hex codes
-    RRGGBBAA = true, -- #RRGGBBAA hex codes
-    rgb_fn = true, -- CSS rgb() and rgba() functions
-    hsl_fn = true, -- CSS hsl() and hsla() functions
-    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-  })
-
-vim.g.gitblame_enabled = 1
-vim.g.gitblame_message_template = "<summary> • <date> • <author>"
-vim.g.gitblame_highlight_group = "LineNr"
-EOL
-
-  echo "#########################################################"
-  echo "Open lvim (e) and run :PackerInstall and :PackerCompile"
-  echo "#########################################################"
-  echo ""
+# Nvchad
+if [ ! -d "$HOME/.config/nvim" ]; then
+  git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+  mkdir -p ~/.config/nvim/lua/custom
+  echo "vim.opt.relativenumber = true" > ~/.config/nvim/lua/custom/init.lua
 fi
+
+# lunarvim
+#export PATH=~/.npm-global/bin:$PATH
+#if [ ! -d "$HOME/.config/lvim" ]; then
+#  mkdir -p $HOME/.npm-global && \
+#  npm config set prefix "$HOME/.npm-global" && \
+#  npm -g install yarn && \
+#  wget -O /tmp/lunar-install.sh https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh && \
+#  yes "y" | bash /tmp/lunar-install.sh && \
+#  rm /tmp/lunar-install.sh && \
+#
+#  cat >> "$HOME/.config/lvim/config.lua" << EOL
+#-- ##################################################
+#-- Run :PackerInstall on first run to install plugins
+#-- ##################################################
+#
+#local formatters = require "lvim.lsp.null-ls.formatters"
+#formatters.setup {
+#  { command = "shfmt", filetypes = { "sh" } },
+#}
+#
+#local linters = require "lvim.lsp.null-ls.linters"
+#linters.setup {
+#  { command = "shellcheck", filetypes = { "sh" } },
+#}
+#
+#vim.opt.relativenumber = true
+#
+#lvim.plugins = {
+#  {"phaazon/hop.nvim"},
+#  {"f-person/git-blame.nvim"},
+#  {"windwp/nvim-ts-autotag"},
+#  {"folke/trouble.nvim", cmd = "TroubleToggle"},
+#  {"folke/todo-comments.nvim"},
+#  {"norcalli/nvim-colorizer.lua"}
+#}
+#
+#local _, hop = pcall(require, "hop")
+#hop.setup()
+#vim.api.nvim_set_keymap("", "s", ":HopChar2<cr>", { silent = true })
+#vim.api.nvim_set_keymap("", "S", ":HopWord<cr>", { silent = true })
+#
+#lvim.builtin.treesitter.autotag = {
+#  enable = true,
+#  filetypes = {'html', 'xml', 'javascript', 'javascriptreact', 'typescriptreact', 'svelte', 'vue', 'rescript'},
+#}
+#
+#-- local _, autotag = pcall(require, "nvim-ts-autotag")
+#-- autotag.setup()
+#
+#local _, todo_comments = pcall(require, "todo-comments")
+#todo_comments.setup()
+#
+#local _, colorizer = pcall(require, "colorizer")
+#colorizer.setup({ "*" }, {
+#    RGB = true, -- #RGB hex codes
+#    RRGGBB = true, -- #RRGGBB hex codes
+#    RRGGBBAA = true, -- #RRGGBBAA hex codes
+#    rgb_fn = true, -- CSS rgb() and rgba() functions
+#    hsl_fn = true, -- CSS hsl() and hsla() functions
+#    css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+#    css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+#  })
+#
+#vim.g.gitblame_enabled = 1
+#vim.g.gitblame_message_template = "<summary> • <date> • <author>"
+#vim.g.gitblame_highlight_group = "LineNr"
+#EOL
+#
+#  echo "#########################################################"
+#  echo "Open lvim (e) and run :PackerInstall and :PackerCompile"
+#  echo "#########################################################"
+#  echo ""
+#fi
 
 #npm list --depth 1 -g neovim > /dev/null || npm install -g neovim
 pip3 show pynvim > /dev/null || sudo python3 -m pip install pynvim
